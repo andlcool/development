c843b4eeddebcd6ac836893e6b10fdf7e9c3f98a
andlcool <andlcool.lau@gmail.com>
2015-07-10 08:54:56 +0800

    check-in into master branch to continue development

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -7,15 +7,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 var renderHomepage = function(req, res, responseBody){
-    var message;
-    if (!(responseBody instanceof Array)) {
-        message = "API lookup error";
-        responseBody = [];
-    } else {
-        if (!responseBody.length) {
-            message = "No places found nearby";
-        }
-    }
     res.render('locations-list', {
         title: 'Loc8r - find a place to work with wifi',
         pageHeader: {
@@ -23,8 +14,7 @@ var renderHomepage = function(req, res, responseBody){
             strapline: 'Find places to work with wifi near you!'
         },
         sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-        locations: responseBody,
-        message: message
+
 
             /* commented out to test API responseBody
             [{
@@ -50,17 +40,7 @@ var renderHomepage = function(req, res, responseBody){
     });
 };
 
-var _formatDistance = function (distance) {
-    var numDistance, unit;
-    if (distance > 1) {
-        numDistance = parseFloat(distance).toFixed(1);
-        unit = 'km';
-    } else {
-        numDistance = parseInt(distance * 1000,10);
-        unit = 'm';
-    }
-    return numDistance + unit;
-};
+
 
 module.exports.homelist = function (req, res) {
     console.log("Homepage loaded");
--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -2,3 +2,58 @@
  * Created by Slevin on 9/7/2015.
  */
 angular.module('loc8rApp', []);
+
+var locationListCtrl = function ($scope) {
+    $scope.data = {
+    locations: [{
+        name: 'Burger Queen',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '0.296456',
+        _id: '5370a35f2536f6785f8dfb6a'
+    },{
+        name: 'Costy',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 5,
+        facilities: ['Hot drinks', 'Food', 'Alcoholic drinks'],
+        distance: '0.7865456',
+        _id: '5370a35f2536f6785f8dfb6b'
+    },{
+        name: 'Rainbow',
+        address: '123 A Street, B, RG6 1PS',
+        rating: 4,
+        facilities: ['Alcoholic drinks', 'Alcoholic', 'drinks'],
+        distance: '0.9865456',
+        _id: '5370a35f2536f6785f8dfb6c'
+    }]};
+};
+
+var _isNumeric = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+var formatDistance = function () {
+    return function (distance){
+        var numDistance, unit;
+        if (distance && _isNumeric(distance)) {
+            if (distance > 1) {
+                numDistance = parseFloat(distance).toFixed(1);
+                unit = 'km';
+            } else {
+                numDistance = parseInt(distance * 1000, 10);
+                unit = 'm';
+            }
+            return numDistance + unit;
+        } else {
+            return "?";
+        }
+    };
+};
+
+
+
+angular
+    .module('loc8rApp')
+    .controller('locationListCtrl', locationListCtrl)
+    .filter('formatDistance', formatDistance);
