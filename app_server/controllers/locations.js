6b881bd46e5b4dfb50a48ebce76d5b595be044be
andlcool <andlcool.lau@gmail.com>
2015-07-10 23:48:55 +0800

    Check-in after completing chap 8

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -7,25 +7,13 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 var renderHomepage = function(req, res, responseBody){
-    var message;
-    if (!(responseBody instanceof Array)) {
-        message = "API lookup error";
-        responseBody = [];
-    } else {
-        if (!responseBody.length) {
-            message = "No places found nearby";
-        }
-    }
     res.render('locations-list', {
         title: 'Loc8r - find a place to work with wifi',
         pageHeader: {
             title: 'Loc8r',
             strapline: 'Find places to work with wifi near you!'
         },
-        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-        locations: responseBody,
-        message: message
-
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for."
             /* commented out to test API responseBody
             [{
             name: 'Starcups',
@@ -50,47 +38,10 @@ var renderHomepage = function(req, res, responseBody){
     });
 };
 
-var _formatDistance = function (distance) {
-    var numDistance, unit;
-    if (distance > 1) {
-        numDistance = parseFloat(distance).toFixed(1);
-        unit = 'km';
-    } else {
-        numDistance = parseInt(distance * 1000,10);
-        unit = 'm';
-    }
-    return numDistance + unit;
-};
+
 
 module.exports.homelist = function (req, res) {
-    console.log("Homepage loaded");
-    var requestOptions, path;
-    path = '/api/locations';
-    requestOptions = {
-        url : apiOptions.server + path,
-        method : "GET",
-        json : {},
-        qs : {
-            lng : -0.98700099,
-            lat : 53.3211,
-            maxDistance : 300
-        }
-    };
-    request(
-        requestOptions,
-        function(err, response, body) {
-            var i, data;
-            data = body;
-            if (response.statusCode === 200 && data.length) {
-                for (i=0; i<data.length; i++) {
-                    data[i].distance = _formatDistance(data[i].distance);
-                }
-            }
-            renderHomepage(req, res, data);
-        }
-    );
-
-
+    renderHomepage(req, res);
 };
 
 
