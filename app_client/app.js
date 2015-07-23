93122cce8198b3ffcc2c6d2240f81effb4eaf416
andlcool <andlcool.lau@gmail.com>
2015-07-23 22:01:03 +0800

    chap 9 mid way check in

--- app_client/app.js
+++ app_client/app.js
@@ -1,3 +1,15 @@
 /**
  * Created by Slevin on 23/7/2015.
  */
+angular.module('loc8rApp', ['ngRoute']);
+
+function config ($routeProvider) {
+    $routeProvider
+        .when('/', {
+        })
+        .otherwise({redirectTo: '/'});
+}
+
+angular
+    .module('loc8rApp')
+    .config(['$routeProvider', config]);
